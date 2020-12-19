827e93fef48f08087c8b8b8e9018f81ffa4fd12c
jangrunicke <jan.grunicke@web.de>
2020-12-19 12:38:04 +0100

    Styling der Vendor-Cards

--- lib/pages/bills/bills_home.dart
+++ lib/pages/bills/bills_home.dart
@@ -3,6 +3,13 @@ import './vendor_card.dart';
 
 class BillsHome extends StatelessWidget {
   var mockData = ['Rewe', 'DM', 'Edeka', 'Penny', 'Aldi'];
+  var colors = [
+    Color.fromRGBO(230, 57, 70, 1.0),
+    Color.fromRGBO(168, 218, 220, 1.0),
+    Color.fromRGBO(254, 168, 168, 1.0),
+    Color.fromRGBO(69, 123, 157, 1.0),
+    Color.fromRGBO(255, 33, 0, 1.0),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +24,7 @@ class BillsHome extends StatelessWidget {
               padding: const EdgeInsets.all(10),
               itemCount: mockData.length,
               itemBuilder: (BuildContext context, int index) {
-                return VendorCard(mockData[index]);
+                return VendorCard(mockData[index], colors[index]);
               }),
         ));
   }
--- lib/pages/bills/vendor_card.dart
+++ lib/pages/bills/vendor_card.dart
@@ -2,19 +2,29 @@ import 'package:flutter/material.dart';
 
 class VendorCard extends StatelessWidget {
   final String vendorName;
+  final Color cardColor;
 
-  VendorCard(this.vendorName);
+  VendorCard(this.vendorName, this.cardColor);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(vendorName),
+      child: Text(
+        vendorName,
+        style:
+            TextStyle(fontSize: 20, color: Color.fromRGBO(250, 250, 250, 1.0)),
+      ),
       decoration: BoxDecoration(
-          shape: BoxShape.rectangle,
-          color: Colors.blue,
-          borderRadius: BorderRadius.circular(20)),
-      alignment: Alignment.topCenter,
-      padding: EdgeInsets.all(10),
+        shape: BoxShape.rectangle,
+        color: cardColor,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+              blurRadius: 4.0, offset: Offset(0.0, 4.0), color: Colors.grey)
+        ],
+      ),
+      alignment: Alignment.topLeft,
+      padding: EdgeInsets.all(15),
       margin: EdgeInsets.all(5),
       constraints: BoxConstraints(maxHeight: 100, maxWidth: 140),
     );
