21651a752dceb33acf552a884857881a8c4f8d12
jangrunicke <jan.grunicke@web.de>
2020-12-19 11:58:20 +0100

    Erstellen einer ListView mit VendorCards

--- lib/pages/bills/bills_home.dart
+++ lib/pages/bills/bills_home.dart
@@ -1,19 +1,24 @@
 import 'package:flutter/material.dart';
+import './vendor_card.dart';
 
 class BillsHome extends StatelessWidget {
+  var mockData = ['Rewe', 'DM', 'Edeka', 'Penny', 'Aldi'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Belege'),
-      ),
-      body: Container(
-        child: Center(
-          child: (
-            Text('Belege Home')
-          ),
-        )
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Belege'),
+        ),
+        body: Container(
+          constraints: BoxConstraints(maxHeight: 140),
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.all(10),
+              itemCount: mockData.length,
+              itemBuilder: (BuildContext context, int index) {
+                return VendorCard(mockData[index]);
+              }),
+        ));
   }
 }
--- lib/pages/bills/vendor_card.dart
+++ lib/pages/bills/vendor_card.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
 class VendorCard extends StatelessWidget {
+  final String vendorName;
+
+  VendorCard(this.vendorName);
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Rewe'),
+      child: Text(vendorName),
       decoration: BoxDecoration(
-        shape: BoxShape.rectangle,
-        color: Colors.blue,
-      ),
+          shape: BoxShape.rectangle,
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(20)),
       alignment: Alignment.topCenter,
-      constraints: BoxConstraints(maxHeight: 80, maxWidth: 120),
+      padding: EdgeInsets.all(10),
+      margin: EdgeInsets.all(5),
+      constraints: BoxConstraints(maxHeight: 100, maxWidth: 140),
     );
   }
 }
