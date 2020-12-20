2fed236dc561a82418b205de433f967a736d0406
jangrunicke <jan.grunicke@web.de>
2020-12-20 14:52:05 +0100

    Versuch Button-Farbe zu wächseln

--- lib/pages/bills/zeitraum_filter_button.dart
+++ lib/pages/bills/zeitraum_filter_button.dart
@@ -6,11 +6,19 @@ class ZeitraumFilterButton extends StatefulWidget {
 }
 
 class _ZeitraumFilterButtonState extends State<ZeitraumFilterButton> {
-  void buttonFn() => {};
-  bool _selected = true;
+  bool _selected = false;
+  void auswaehlen() {
+    print('Ausgewählt?: ' + _selected.toString());
+    setState(() {
+      _selected = true;
+    });
+    print('Button wurde ausgewählt');
+    print('Ausgewählt?: ' + _selected.toString());
+  }
+
   final colors = [
     Color.fromRGBO(224, 224, 244, 1.0),
-    Color.fromRGBO(29, 53, 87, 1.0)
+    Color.fromRGBO(29, 53, 87, 1.0),
   ];
 
   @override
@@ -18,11 +26,10 @@ class _ZeitraumFilterButtonState extends State<ZeitraumFilterButton> {
     return Container(
       child: FlatButton(
         child: Text('1 Woche'),
-        onPressed: () => {},
+        onPressed: () => {auswaehlen()},
       ),
       decoration: BoxDecoration(
-          color: _selected ? colors[1] : colors[2],
-          borderRadius: BorderRadius.circular(15)),
+          color: colors[0], borderRadius: BorderRadius.circular(15)),
       constraints: BoxConstraints(maxHeight: 35, maxWidth: 110),
     );
   }
