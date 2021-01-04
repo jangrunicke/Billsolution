8594b9d6bd649713c1cd680ca7d08444ee38f8e3
jangrunicke <jan.grunicke@web.de>
2021-01-04 20:50:06 +0100

    Liste zu Beleg-Details angepasst

--- lib/pages/bills/bill_details.dart
+++ lib/pages/bills/bill_details.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 
 class BillDetails extends StatelessWidget {
+  var mockData = [
+    {'name': 'Weihnstephan Milch', 'anzahl': 10, 'preis': 20.87},
+    {'name': 'Bergbauernmilch', 'anzahl': 10, 'preis': 13.99},
+    {'name': 'Chiqita Banane', 'anzahl': 3, 'preis': 2.99},
+    {'name': 'Zahnpasta', 'anzahl': 1, 'preis': 3.29},
+  ];
+
   Widget buildGroup(String gruppenname) {
     return Scaffold(
         appBar: AppBar(
@@ -11,13 +18,22 @@ class BillDetails extends StatelessWidget {
         body: Column(
           children: [
             Container(
-              child: Text(
-                'Gruppe 1',
-                style: TextStyle(
-                    color: Color.fromRGBO(152, 152, 152, 1.0), fontSize: 18),
+              child: Column(
+                children: [
+                  Text(
+                    'Gruppe 1',
+                    style: TextStyle(
+                        color: Color.fromRGBO(152, 152, 152, 1.0),
+                        fontSize: 16),
+                  ),
+                  SizedBox(
+                    height: 400,
+                    child: _buildBeleg(),
+                  )
+                ],
               ),
               margin: EdgeInsets.all(15),
-              padding: EdgeInsets.all(10),
+              padding: EdgeInsets.all(20),
               decoration: BoxDecoration(
                 shape: BoxShape.rectangle,
                 color: Colors.white,
@@ -33,9 +49,25 @@ class BillDetails extends StatelessWidget {
                 width: double.infinity,
                 height: 500,
               ),
-            )
+            ),
           ],
         ));
+  }
+
+  Widget _buildBeleg() {
+    return ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemCount: mockData.length,
+        itemBuilder: (context, i) {
+          return _buildPosition(i);
+        });
+  }
+
+  Widget _buildPosition(int index) {
+    return ListTile(
+      title: Expanded(child: Text(mockData[index]['name'])),
+      trailing: Text(mockData[index]['preis'].toString()),
+    );
   }
 
   @override
