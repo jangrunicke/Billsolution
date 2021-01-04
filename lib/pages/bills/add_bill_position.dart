640dba60793e32424df23181954a0fbeaedb1cd2
Roland Lindt <liro1017@hs-karlsruhe.de>
2021-01-04 19:38:19 +0100

    Seite zum Hinzufügen von Positionen erstellt

---
+++ lib/pages/bills/add_bill_position.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import './widgets/add_bill_widgets.dart';
+import './add_position_details.dart';
+
+class AddBillPosition extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AddBillPositionAppBar(
+        'Positionen',
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(35, 35, 35, 15),
+        child: Container(
+          width: 426,
+          height: 48,
+          child: RaisedButton(
+            elevation: 0,
+            color: Color.fromARGB(110, 224, 224, 224),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+            onPressed: () {
+              Route route = MaterialPageRoute(
+                  builder: (context) => AddBillPositionDetails());
+              Navigator.push(context, route);
+            },
+            child: Icon(
+              Icons.add,
+              size: 20,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
