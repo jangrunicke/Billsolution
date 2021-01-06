7fb4c3a9caf7f6fb913227eb04b55311a739089b
jangrunicke <jan.grunicke@web.de>
2021-01-06 23:00:54 +0100

    Bill Details Stream erstellt

--- lib/pages/bills/bill_details.dart
+++ lib/pages/bills/bill_details.dart
@@ -1,79 +1,53 @@
+import 'package:billsolution_app/pages/analytics/mock/billposition_class.dart';
+import 'package:billsolution_app/pages/bills/mock/billposition_stream2.dart';
 import 'package:flutter/material.dart';
 
-class BillDetails extends StatelessWidget {
-  final mockData = [
-    {'name': 'Weihnstephan Milch', 'anzahl': 10, 'preis': 20.87},
-    {'name': 'Bergbauernmilch', 'anzahl': 10, 'preis': 13.99},
-    {'name': 'Chiqita Banane', 'anzahl': 3, 'preis': 2.99},
-    {'name': 'Zahnpasta', 'anzahl': 1, 'preis': 3.29},
-  ];
+class BillDetails extends StatefulWidget {
+  // final mockData = [
+  //   {'name': 'Weihnstephan Milch', 'anzahl': 10, 'preis': 20.87},
+  //   {'name': 'Bergbauernmilch', 'anzahl': 10, 'preis': 13.99},
+  //   {'name': 'Chiqita Banane', 'anzahl': 3, 'preis': 2.99},
+  //   {'name': 'Zahnpasta', 'anzahl': 1, 'preis': 3.29},
+  // ];
 
-  Widget buildGroup(String gruppenname) {
+  @override
+  _BillDetailsState createState() => _BillDetailsState();
+}
+
+class _BillDetailsState extends State<BillDetails> {
+  final Stream<List<Billposition>> _positions =
+      Stream<List<Billposition>>.fromIterable(<List<Billposition>>[
+    [
+      Billposition(
+          name: 'Zahnpasta', amount: 10, price: 1.12, category: 'Hygiene'),
+      Billposition(
+          name: 'Milch', amount: 2, price: 1.42, category: 'Lebensmittel'),
+      Billposition(
+          name: 'Paprika', amount: 1, price: 1.02, category: 'Lebensmittel')
+    ].toList()
+  ]);
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Column(
             children: [Text('Rewe Einkauf'), Text('27.12.2020')],
           ),
         ),
-        body: Column(
-          children: [
-            Container(
-              child: Column(
-                children: [
-                  Text(
-                    'Gruppe 1',
-                    style: TextStyle(
-                        color: Color.fromRGBO(152, 152, 152, 1.0),
-                        fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: 400,
-                    child: _buildBeleg(),
-                  )
-                ],
-                mainAxisAlignment: MainAxisAlignment.start,
-              ),
-              margin: EdgeInsets.all(15),
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                shape: BoxShape.rectangle,
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(20),
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 4.0,
-                      offset: Offset(0.0, 4.0),
-                      color: Colors.grey)
-                ],
-              ),
-              constraints: BoxConstraints.expand(
-                width: double.infinity,
-                height: 500,
-              ),
-            ),
-          ],
-        ));
-  }
-
-  Widget _buildBeleg() {
-    return ListView.builder(
-        padding: EdgeInsets.all(16.0),
-        itemCount: mockData.length,
-        itemBuilder: (context, i) {
-          return _buildPosition(i);
-        });
-  }
-
-  Widget _buildPosition(int index) {
-    return ListTile(
-      title: Expanded(child: Text(mockData[index]['name'])),
-      trailing: Text(mockData[index]['preis'].toString()),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return buildGroup('Gruppe 1');
-    // return Scaffold(appBar: AppBar(title: Text('bla')), body: Container());
+        body: Container(
+            child: StreamBuilder(
+          stream: _positions,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('Error');
+            }
+            if (snapshot.hasData) {
+              return Text(snapshot.data[1].name);
+            } else {
+              return Text('Nichts');
+            }
+          },
+        )));
   }
 }
