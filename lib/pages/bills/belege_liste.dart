070abac7e39c1d9fdf408b4aa34bcb60d6a122a1
jangrunicke <jan.grunicke@web.de>
2021-01-07 15:09:48 +0100

    Belege List auf Firebase umgestellt

--- lib/pages/bills/belege_liste.dart
+++ lib/pages/bills/belege_liste.dart
@@ -1,35 +1,63 @@
+import 'package:billsolution_app/aggregates/bill/bill.dart';
+import 'package:billsolution_app/aggregates/user.dart';
 import 'package:billsolution_app/pages/bills/bill_details.dart';
+import 'package:billsolution_app/user_model.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class BelegeListe extends StatefulWidget {
-  @override
-  _BelegeListeState createState() => _BelegeListeState();
-}
-
-class _BelegeListeState extends State<BelegeListe> {
-  Widget _buildBelege() {
-    return ListView.builder(
-        shrinkWrap: true,
-        padding: EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return Divider();
-
-          final index = i ~/ 2;
-          return _buildRow();
-        });
+class BelegeListe extends StatelessWidget {
+  Widget _buildBelege(BuildContext context) {
+    return Container(
+      child: Consumer<UserModel>(builder: (context, user, child) {
+        return StreamBuilder(
+          stream: user.user,
+          builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+            if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            }
+            if (!snapshot.hasData) {
+              return Text('User ist leer');
+            }
+            return StreamBuilder(
+                stream: snapshot.data.getBills(),
+                builder:
+                    (BuildContext context, AsyncSnapshot<List<Bill>> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
+                  }
+                  // if (!snapshot.hasData) {
+                  //   return Text('Empty');
+                  // }
+                  return _buildBillListView(snapshot.data, context);
+                });
+          },
+        );
+      }),
+    );
   }
 
-  Widget _buildRow() {
+  Widget _buildBillListView(List<Bill> bills, BuildContext context) {
+    return ListView.separated(
+      padding: EdgeInsets.all(10),
+      itemCount: bills.length,
+      itemBuilder: (BuildContext context, int index) {
+        return _buildBillListTile(bills[index], context);
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    );
+  }
+
+  Widget _buildBillListTile(Bill bill, BuildContext context) {
     return ListTile(
         title: Row(
           children: [
             Expanded(
                 child: Column(
               children: [
-                Text("Händler",
+                Text(bill.shop.name,
                     style:
                         TextStyle(fontWeight: FontWeight.bold, fontSize: 18)),
-                Text("27.12.2020",
+                Text(_toDateFormat(bill.created_at),
                     style: TextStyle(color: Color.fromRGBO(152, 152, 152, 1.0)))
               ],
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -53,8 +81,17 @@ class _BelegeListeState extends State<BelegeListe> {
         });
   }
 
+  String _toDateFormat(DateTime date) {
+    var result = date.day.toString() +
+        '.' +
+        date.month.toString() +
+        '.' +
+        date.year.toString();
+    return result;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Expanded(child: _buildBelege());
+    return Expanded(child: _buildBelege(context));
   }
 }
