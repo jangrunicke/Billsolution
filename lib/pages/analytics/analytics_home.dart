bf930d25fa397f3a1b0f06bace1636d52464e77a
Philipp Sonntag <jan-philipp@sonntagncp.de>
2021-01-07 18:32:06 +0100

    analytics Charts_Demo begonnen

--- lib/pages/analytics/analytics_home.dart
+++ lib/pages/analytics/analytics_home.dart
@@ -1,64 +1,53 @@
 import 'dart:async';
 
+import 'package:billsolution_app/components/primary_button.dart';
+import 'package:billsolution_app/pages/analytics/analytics_graphic.dart';
+import 'package:billsolution_app/pages/analytics/analytics_router.dart';
 import 'package:billsolution_app/pages/analytics/mock/billposition_class.dart';
 import 'package:billsolution_app/pages/analytics/mock/billposition_stream.dart';
 import 'package:flutter/material.dart';
+import 'package:graphic/graphic.dart' as graphic;
 
 class AnalyticsHome extends StatelessWidget {
   Stream stream = new BillpositionStream().getStream();
 
-  List<Widget> buildList() {
-    return [
-      Container(
-        child: StreamBuilder(
-          stream: stream,
-          builder: (BuildContext context, AsyncSnapshot snapshot) {
-            if (snapshot.hasError) {
-              return Text('Error');
-            }
-            if (snapshot.hasData) {
-              return Text(snapshot.data.name);
-            } else {
-              return Text('Empty');
-            }
-          },
-        ),
-      )
-    ];
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 1,
-        backgroundColor: Color.fromARGB(240, 249, 249, 249),
-        title: Text(
-          'Auswertung',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 25,
-            fontFamily: 'Avenir Next',
+        appBar: AppBar(
+          title: Text(
+            'Auswertung',
           ),
-          textAlign: TextAlign.center,
+          actions: [
+            IconButton(
+                icon: Icon(
+                  Icons.more_horiz,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  return null;
+                }),
+          ],
         ),
-        toolbarHeight: 69.0,
-        actions: [
-          IconButton(
-              icon: Icon(
-                Icons.more_horiz,
-                color: Color.fromARGB(255, 29, 53, 87),
-              ),
-              onPressed: () {
-                return null;
-              }),
-        ],
-      ),
-      body: Container(
-        child: Center(
-          child: ListView(children: buildList()),
-        ),
-      ),
-    );
+        body: Center(
+          child: Container(
+            child: Padding(
+                padding: const EdgeInsets.all(36.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                        width: 350,
+                        height: 500,
+                        child: Container(
+                            height: 20,
+                            child: ChartsDemo(
+                              betrag: 140,
+                            ))),
+                  ],
+                )),
+          ),
+        ));
   }
 }
