ce5229a5103e3882b5ad102e2014ba035085a62c
Philipp Sonntag <jan-philipp@sonntagncp.de>
2021-01-08 14:32:57 +0100

    analytics Stream behandlung

--- lib/pages/analytics/analytics_home.dart
+++ lib/pages/analytics/analytics_home.dart
@@ -1,53 +1,61 @@
 import 'dart:async';
 
-import 'package:billsolution_app/components/primary_button.dart';
-import 'package:billsolution_app/pages/analytics/analytics_graphic.dart';
-import 'package:billsolution_app/pages/analytics/analytics_router.dart';
-import 'package:billsolution_app/pages/analytics/mock/billposition_class.dart';
-import 'package:billsolution_app/pages/analytics/mock/billposition_stream.dart';
+import 'package:billsolution_app/aggregates/bill/bill.dart';
+import 'package:billsolution_app/aggregates/billposition/billposition.dart';
+import 'package:billsolution_app/aggregates/user.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:graphic/graphic.dart' as graphic;
+import 'package:provider/provider.dart';
+
+import '../../user_model.dart';
 
 class AnalyticsHome extends StatelessWidget {
-  Stream stream = new BillpositionStream().getStream();
+  String aggreagtion(Stream<List<Bill>> stream, String category) {
+    String test = stream.first.toString();
+
+    final subscrition = stream.listen((event) {
+      test = event.first.id;
+    });
+    return test;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            'Auswertung',
-          ),
-          actions: [
-            IconButton(
-                icon: Icon(
-                  Icons.more_horiz,
-                  color: Colors.white,
-                ),
-                onPressed: () {
-                  return null;
-                }),
-          ],
+      appBar: AppBar(
+        title: Text(
+          'Auswertung',
         ),
-        body: Center(
-          child: Container(
-            child: Padding(
-                padding: const EdgeInsets.all(36.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                        width: 350,
-                        height: 500,
-                        child: Container(
-                            height: 20,
-                            child: ChartsDemo(
-                              betrag: 140,
-                            ))),
-                  ],
-                )),
-          ),
-        ));
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.more_horiz,
+                color: Colors.white,
+              ),
+              onPressed: () {
+                return null;
+              }),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          child: Consumer<UserModel>(builder: (context, user, child) {
+            return StreamBuilder(
+              stream: user.user,
+              builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+                if (snapshot.hasError) {
+                  return Text(snapshot.error.toString());
+                }
+                if (snapshot.hasData) {
+                  return Text(aggreagtion(snapshot.data.getBills(), 'hallo'));
+                } else {
+                  return Text('Empty');
+                }
+              },
+            );
+          }),
+        ),
+      ),
+    );
   }
 }
