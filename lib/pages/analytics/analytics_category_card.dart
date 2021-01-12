db1f9f6b8cbd15e94dcb51faeda939f641a49376
Philipp Sonntag <jan-philipp@sonntagncp.de>
2021-01-12 20:10:39 +0100

    analytics dynamic Bar Charts

--- lib/pages/analytics/analytics_category_card.dart
+++ lib/pages/analytics/analytics_category_card.dart
@@ -1,93 +1,76 @@
-import 'package:billsolution_app/aggregates/bill/bill.dart';
-import 'package:billsolution_app/aggregates/billposition/billposition.dart';
+import 'dart:math';
+
 import 'package:billsolution_app/aggregates/user.dart';
-import 'package:billsolution_app/pages/analytics/analytics_graphic.dart';
+import 'package:billsolution_app/components/secondary_button.dart';
+import 'package:billsolution_app/pages/analytics/analytics_graphic_card.dart';
+import 'package:billsolution_app/pages/analytics/widgets/analytics_details_button.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import '../../user_model.dart';
 
 class AnalyticsCategoryCard extends StatelessWidget {
   final String category;
 
   AnalyticsCategoryCard({this.category});
 
+  double roundDouble(String ergebnis) {
+    double sum = double.parse(ergebnis);
+    double mod = pow(10.0, 2);
+    return ((sum * mod).round().toDouble() / mod);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.all(8),
-        child: Container(
-          width: 500,
-          height: 125,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: Colors.white,
-            boxShadow: [
-              BoxShadow(
-                color: Colors.grey,
-                offset: Offset(0.0, 1.0),
-                blurRadius: 6.0,
-              ),
-            ],
+      padding: EdgeInsets.all(8),
+      child: Container(
+        width: 500,
+        height: 184,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey,
+              offset: Offset(0.0, 1.0),
+              blurRadius: 6.0,
+            ),
+          ],
+        ),
+        child: Center(
+          child: Consumer<User>(
+            builder: (context, user, child) {
+              return StreamBuilder(
+                stream: user.calculateSumOfCategory(category),
+                builder: (context, AsyncSnapshot<double> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text(snapshot.error.toString());
+                  }
+                  if (!snapshot.hasData) {
+                    return Text('Empty');
+                  }
+                  return Column(children: <Widget>[
+                    SizedBox(height: 5),
+                    Text(
+                      category,
+                      textAlign: TextAlign.left,
+                    ),
+                    SizedBox(height: 5),
+                    Text(roundDouble(snapshot.data.toString()).toString()),
+                    AnalyticsGraphicCard(
+                      stream: user.calculateSumOfCategory(category),
+                    ),
+                    SizedBox(height: 50.0),
+                    AnalyticsDetailsButton(
+                      onPressed: null,
+                      text: 'Details',
+                    )
+                  ]);
+                },
+              );
+            },
           ),
-          child: Column(children: [
-            Text(
-              category,
-            ),
-            Consumer<UserModel>(
-              builder: (context, user, child) {
-                return StreamBuilder(
-                    stream: user.user,
-                    builder:
-                        (BuildContext context, AsyncSnapshot<User> snapshot) {
-                      if (snapshot.hasError) {
-                        return Text(snapshot.error.toString());
-                      }
-                      if (!snapshot.hasData) {
-                        return Text('Empty');
-                      }
-                      return StreamBuilder(
-                          stream: snapshot.data.getBills(),
-                          builder: (BuildContext context,
-                              AsyncSnapshot<List<Bill>> snapshot) {
-                            if (snapshot.hasError) {
-                              return Text(snapshot.error.toString());
-                            }
-                            if (!snapshot.hasData) {
-                              return Text('Empty');
-                            }
-                            // return Text(snapshot.data.toString());
-                            return StreamBuilder(
-                              stream: snapshot.data.first
-                                  .getCalculatedSumOfCategory(category),
-                              builder: (BuildContext context,
-                                  AsyncSnapshot<double> snapshot) {
-                                if (snapshot.hasError) {
-                                  return Text(snapshot.error.toString());
-                                }
-                                if (!snapshot.hasData) {
-                                  return Text('Empty');
-                                }
-                                return Column(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Text(
-                                      snapshot.data.toString(),
-                                      style: TextStyle(fontSize: 15),
-                                    ),
-                                    // ChartsDemo(
-                                    //   betrag: snapshot.data.toString(),
-                                    // ),
-                                  ],
-                                );
-                              },
-                            );
-                          });
-                    });
-              },
-            ),
-          ]),
-        ));
+        ),
+      ),
+    );
   }
 }
---
+++ lib/pages/analytics/analytics_graphic_card.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class AnalyticsGraphicCard extends StatelessWidget {
+  Stream<double> stream;
+  AnalyticsGraphicCard({this.stream});
+
+  static double _getReferenz(double betrag) {
+    int referenz = (betrag ~/ 100);
+    return (referenz * 100 + 100).toDouble();
+  }
+
+  static List<charts.Series<Billposition, String>> _generateData(
+      String betrag) {
+    double doubleBetrag = double.parse(betrag);
+    final desktopSaleData = [
+      Billposition(category: '', price: _getReferenz(doubleBetrag)),
+      Billposition(category: '', price: doubleBetrag),
+    ];
+    return [
+      charts.Series<Billposition, String>(
+        id: 'Sales',
+        domainFn: (Billposition billposition, _) => billposition.category,
+        measureFn: (Billposition billposition, _) => billposition.price,
+        data: desktopSaleData,
+      )
+    ];
+  }
+
+  barchart(String streamString) {
+    return charts.BarChart(
+      _generateData(streamString),
+      animate: true,
+      vertical: false,
+      domainAxis: charts.OrdinalAxisSpec(renderSpec: charts.NoneRenderSpec()),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: stream,
+      builder: (context, AsyncSnapshot<double> snapshot) {
+        return Container(
+            width: 350, height: 22, child: barchart(snapshot.data.toString()));
+      },
+    );
+  }
+}
+
+class Billposition {
+  final String category;
+  final double price;
+
+  Billposition({this.category, this.price});
+}
