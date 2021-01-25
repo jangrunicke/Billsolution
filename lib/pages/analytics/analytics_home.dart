f051ad7c70c561f51ccc0ff0d6ac55237cf7038e
Philipp Sonntag <jan-philipp@sonntagncp.de>
2021-01-25 11:50:59 +0100

    PieChart fix

--- lib/pages/analytics/analytics_home.dart
+++ lib/pages/analytics/analytics_home.dart
@@ -8,26 +8,27 @@ class AnalyticsHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromRGBO(249, 249, 249, 1.0),
-        appBar: AppBar(
-          title: Text(
-            'Auswertung',
-          ),
+      backgroundColor: Color.fromRGBO(249, 249, 249, 1.0),
+      appBar: AppBar(
+        title: Text(
+          'Auswertung',
         ),
-        body: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              AnalyticsCategoryCard(
-                category: 'Gesamt',
-                color: Theme.of(context).primaryColor,
-              ),
-              AnalyticsPiChart(),
-              Expanded(
-                child: AnalyticsCategoryList(),
-              )
-            ],
-          ),
-        ));
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            AnalyticsCategoryCard(
+              category: 'Gesamt',
+              color: Theme.of(context).primaryColor,
+            ),
+            AnalyticsPiChart(),
+            Expanded(
+              child: AnalyticsCategoryList(),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/pages/analytics/analytics_pichart.dart
+++ lib/pages/analytics/analytics_pichart.dart
@@ -1,4 +1,3 @@
-import 'dart:collection';
 import 'dart:math';
 import 'package:billsolution_app/aggregates/user.dart';
 import 'package:flutter/material.dart';
@@ -6,23 +5,6 @@ import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:provider/provider.dart';
 
 class AnalyticsPiChart extends StatelessWidget {
-  List<String> _getDistinctCategories(List<String> cats) {
-    var categorynames = List<String>();
-    cats.forEach((category) {
-      if (!categorynames.contains(category)) {
-        categorynames.add(category);
-      }
-    });
-    categorynames.sort();
-    return categorynames;
-  }
-
-  static double _roundDouble(String ergebnis) {
-    double sum = double.parse(ergebnis);
-    double mod = pow(10.0, 2);
-    return ((sum * mod).round().toDouble() / mod);
-  }
-
   List<PiChartPos> setcolors(List<PiChartPos> pichart) {
     int counter = 0;
 
@@ -51,17 +33,11 @@ class AnalyticsPiChart extends StatelessWidget {
 
   List<charts.Series<PiChartPos, String>> _generateData(
       List<PiChartPos> pichartData) {
-    var mockdata = [
-      PiChartPos(categorty: 'Auto', sum: 20),
-      PiChartPos(categorty: 'Büroartikel', sum: 50),
-    ];
-
-    var colormockdata = setcolors(pichartData);
+    var colorizeddata = setcolors(pichartData);
     return [
       charts.Series<PiChartPos, String>(
         id: 'Pichart',
-        data: colormockdata,
-        //data: pichartData,
+        data: colorizeddata,
         domainFn: (PiChartPos pichartpos, _) => pichartpos.categorty,
         measureFn: (PiChartPos pichartpos, _) => pichartpos.sum,
         colorFn: (PiChartPos pichartpos, _) =>
@@ -88,8 +64,7 @@ class AnalyticsPiChart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 150,
-      // child: pichart([]),
+      height: 200,
       child: Consumer<User>(
         builder: (context, user, child) {
           if (user == null) {
